554dfe7559a35ea20c0897332264f88e1c2f29bc
shelrod <shelrod@pc27>
2012-09-04 11:24:08 +0530

    Extract method amountfor() in statement()

--- RefactoringDemo/src/Customer.java
+++ RefactoringDemo/src/Customer.java
@@ -22,21 +22,7 @@ public class Customer {
 		double thisAmount = 0;
 		Rental each = (Rental) rentals.nextElement();
 		//determine amounts for each line
-		switch (each.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-		thisAmount += 2;
-		if (each.getDaysRented() > 2)
-		thisAmount += (each.getDaysRented() - 2) * 1.5;
-		break;
-		case Movie.NEW_RELEASE:
-		thisAmount += each.getDaysRented() * 3;
-		break;
-		case Movie.CHILDRENS:
-		thisAmount += 1.5;
-		if (each.getDaysRented() > 3)
-		thisAmount += (each.getDaysRented() - 3) * 1.5;
-		break;
-		}
+		thisAmount = amountFor(each);
 		// add frequent renter points
 		frequentRenterPoints ++;
 		// add bonus for a two day new release rental
@@ -57,4 +43,23 @@ public class Customer {
 		return result;
 
 }
+	private double amountFor(Rental each) {
+		double thisAmount = 0;
+		switch (each.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+		thisAmount += 2;
+		if (each.getDaysRented() > 2)
+		thisAmount += (each.getDaysRented() - 2) * 1.5;
+		break;
+		case Movie.NEW_RELEASE:
+		thisAmount += each.getDaysRented() * 3;
+		break;
+		case Movie.CHILDRENS:
+		thisAmount += 1.5;
+		if (each.getDaysRented() > 3)
+		thisAmount += (each.getDaysRented() - 3) * 1.5;
+		break;
+		}
+		return thisAmount;
+	}
 }
